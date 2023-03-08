90f581eb5dbf9bdc84e104bf63ecea01084ab613
UnaiGMorchon <unai@MacBook-Pro-de-Unai.local>
2023-03-08 16:03:37 +0100

    actualizado html

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
 
 
-// 30 preguntas de historia de la API
-fetch('https://opentdb.com/api.php?amount=30&category=23&difficulty=easy&type=multiple')
+// 5 preguntas de historia de la API
+fetch('https://opentdb.com/api.php?amount=5&category=23&difficulty=easy&type=multiple')
   .then(response => response.json())
   .then(data => console.log(data))
   .catch(error => console.error(error))
 
- // 30 preguntas de arte de la API
-fetch('https://opentdb.com/api.php?amount=30&category=25&difficulty=easy&type=multiple')
+ // 5 preguntas de arte de la API
+fetch('https://opentdb.com/api.php?amount=5&category=25&difficulty=easy&type=multiple')
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error))
         
- // 30 preguntas de ciencia naturaleza de la API
- fetch('https://opentdb.com/api.php?amount=30&category=17&difficulty=easy&type=multiple')
+ // 5 preguntas de ciencia naturaleza de la API
+ fetch('https://opentdb.com/api.php?amount=5&category=17&difficulty=easy&type=multiple')
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error))
     
 
+
+    
