366e255df7859f68a6726fbb526806ec2e0f9b5c
UnaiGMorchon <unai@MacBook-Pro-de-Unai.local>
2023-03-09 12:47:02 +0100

    preguntas y respuestas ok

--- script.js
+++ script.js
@@ -1,87 +1,64 @@
 
-
-
 // 5 preguntas de historia de la API
+
+let categories={
+    "history": 23,
+    "ciencia": 17,
+    "arte": 25
+}
+
+function getQuestions(){
+for (let i = 0; i < categories.length; i++) {
+    const element = array[i];
+    return element;
+}
+}
+let category = categories["ciencia","history","arte"];
+
+
 let questions =[];
 let questionNumber =0;
 let correctAnswer =[];
 let correctAnswerNumber =0;
-fetch('https://opentdb.com/api.php?amount=5&category=23&difficulty=easy&type=multiple')
+fetch("https://opentdb.com/api.php?amount=5&category=" + category + "&difficulty=easy&type=multiple")
   .then(response => response.json())
   .then(data => {
     questions = data.results;
     createQuestion();
-   
 })
   .catch(error => {
   console.error(error)
 });
-
 function createQuestion(){
     let question = questions[questionNumber];
     console.log(question);
     document.getElementById("question").innerText = question.question;
     let answers = question.incorrect_answers;
-    answers.push(question.correct_answer);
+    answers.unshift(question.correct_answer);
+
+// Desordenar aleatoriamente las respuestas
+shuffle(answers);
 
     answers.forEach(answer => {
         let h2 = document.createElement("h2");
         h2.innerText = answer;
         document.getElementById("choices").appendChild(h2);
-
     });
-
-
 }
 
+function shuffle(array) {
+    for (let i = array.length -1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
 
 
 
 
 
 
-
-
-
-/* 
-
-// 5 preguntas de historia de la API
-let questions =[];
-let questionNumber =0;
-fetch('https://opentdb.com/api.php?amount=5&category=23&difficulty=easy&type=multiple')
-  .then(response => response.json())
-  .then(data => {
-    questions = data.results;
-    createQuestion();
-})
-  .catch(error => {
-  console.error(error)
-});
-
-function createQuestion(){
-    let question = questions[questionNumber];
-    console.log(question);
-    document.getElementById("question").innerText = question.question;
-}
-
-
-let correctAnswer =[];
-let correctAnswerNumber =0;
-fetch('https://opentdb.com/api.php?amount=5&category=23&difficulty=easy&type=multiple')
-  .then(response => response.json())
-  .then(data => {
-    correctAnswer = data.results;
-    createCorrectAnswer();
-})
-  .catch(error => {
-  console.error(error)
-});
-
-function createCorrectAnswer(){
-    let correct = correctAnswer[correctAnswerNumber];
-    console.log(correct);
-    document.getElementById("answer1").innerText = correct.correct_answer;
-} */
 
 
 
